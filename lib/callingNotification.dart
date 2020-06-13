383e0372deaea7221f2000bdd87a7cf62f50651b
Im-awesome-Aadi <43954262+Im-awesome-Aadi@users.noreply.github.com>
2020-06-13 17:16:22 +0530

    Update callingNotification.dart

--- lib/callingNotification.dart
+++ lib/callingNotification.dart
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
+/*
+=======
+>>>>>>> origin/master
 import 'dart:io';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:audioplayers/audioplayers.dart';
@@ -5,11 +9,72 @@ import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:convert';
+<<<<<<< HEAD
+import 'package:alarm_demo/systemSpecificPaths.dart';
+void callingNotification(int uniqueID) async {
+
+    //For Daily Alarm
+
+    print("$uniqueID is called");
+    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+        FlutterLocalNotificationsPlugin();
+    var initializationSettings =  notificationInitializationSettings();
+    flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+    );
+
+    var platform =  await notificationSettings();
+    await flutterLocalNotificationsPlugin.show(
+        0, "Fitness traker", "gh", platform,
+        payload: "You have clicked notification");
+
+
+}
+
+InitializationSettings notificationInitializationSettings() {
+  var androids = AndroidInitializationSettings('bvplogo'); //Notification Icon
+  var iOS = IOSInitializationSettings();
+  var initializationSettings = InitializationSettings(androids, iOS);
+  return initializationSettings;
+}
+
+Future  notificationSettings() async{
+
+  var android = AndroidNotificationDetails(
+    "channelID",
+    "channelName",
+    "channelDescription",
+    playSound: true,
+    enableLights: true,
+    enableVibration: true,
+    color: Colors.green,
+    importance: Importance.Max,
+    priority: Priority.High,
+
+  );
+  var ios = IOSNotificationDetails();
+  var platform = NotificationDetails(android, ios);
+  return platform;
+}
+*/
+
+
+import 'dart:io';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:convert';
+import 'package:alarm_demo/systemSpecificPaths.dart';
+=======
+>>>>>>> origin/master
 void callingNotification(int uniqueID) async {
   final prefs = await SharedPreferences.getInstance();
   final value = prefs.getString(uniqueID.toString());
   var temp = json.decode(value);
   print("Ima ");
+  prefs.setString("demo", "12347");
   print(uniqueID);
   print(uniqueID.runtimeType);
   var schedule = uniqueID.toString().substring(
@@ -31,14 +96,18 @@ void callingNotification(int uniqueID) async {
         0, "Fitness traker", "${temp["label"]}", platform,
         payload: "You have clicked notification");
     AudioPlayer audioPlayer2 = AudioPlayer();
+    Directory appDocDirectory;
+    appDocDirectory = await systemSpecificFilePath();
     var audioFilePath =
-        "/storage/emulated/0/Android/data/com.aditya25dev.alarm_demo/files/" +
-            "${uniqueID.toString()}" +
+        appDocDirectory.path +
+            "/${uniqueID.toString()}" +
             ".wav";
     print("I called $audioFilePath");
     await audioPlayer2.play(audioFilePath, isLocal: true);
     final DateTime now = DateTime.now();
-  } else {
+  } 
+  // This condtion is not used now. Ignore it for a while
+  else {
     print(schedule);
     print("not laone");
     schedule.runes.forEach((int rune) async {
@@ -84,12 +153,6 @@ InitializationSettings notificationInitializationSettings() {
 }
 
 Future  notificationSettings(img) async{
-  print(img);
-  Directory  appDocDirectory;
-  appDocDirectory = await getExternalStorageDirectory();
-  var a= appDocDirectory.path + '/Pictures/D.png';
-  print(a);
-
 
   var android = AndroidNotificationDetails(
     "channelID",
@@ -110,3 +173,4 @@ Future  notificationSettings(img) async{
   var platform = NotificationDetails(android, ios);
   return platform;
 }
+
