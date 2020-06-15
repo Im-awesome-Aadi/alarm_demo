582520f89ad7697e7566be520f5db8fcba3da1b0
Im-awesome-Aadi <43954262+Im-awesome-Aadi@users.noreply.github.com>
2020-06-15 14:04:58 +0530

    Update callingNotification.dart

--- lib/callingNotification.dart
+++ lib/callingNotification.dart
@@ -1,64 +1,3 @@
-<<<<<<< HEAD
-/*
-=======
->>>>>>> origin/master
-import 'dart:io';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:audioplayers/audioplayers.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_local_notifications/flutter_local_notifications.dart';
-import 'package:path_provider/path_provider.dart';
-import 'dart:convert';
-<<<<<<< HEAD
-import 'package:alarm_demo/systemSpecificPaths.dart';
-void callingNotification(int uniqueID) async {
-
-    //For Daily Alarm
-
-    print("$uniqueID is called");
-    FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-        FlutterLocalNotificationsPlugin();
-    var initializationSettings =  notificationInitializationSettings();
-    flutterLocalNotificationsPlugin.initialize(
-      initializationSettings,
-    );
-
-    var platform =  await notificationSettings();
-    await flutterLocalNotificationsPlugin.show(
-        0, "Fitness traker", "gh", platform,
-        payload: "You have clicked notification");
-
-
-}
-
-InitializationSettings notificationInitializationSettings() {
-  var androids = AndroidInitializationSettings('bvplogo'); //Notification Icon
-  var iOS = IOSInitializationSettings();
-  var initializationSettings = InitializationSettings(androids, iOS);
-  return initializationSettings;
-}
-
-Future  notificationSettings() async{
-
-  var android = AndroidNotificationDetails(
-    "channelID",
-    "channelName",
-    "channelDescription",
-    playSound: true,
-    enableLights: true,
-    enableVibration: true,
-    color: Colors.green,
-    importance: Importance.Max,
-    priority: Priority.High,
-
-  );
-  var ios = IOSNotificationDetails();
-  var platform = NotificationDetails(android, ios);
-  return platform;
-}
-*/
-
-
 import 'dart:io';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:audioplayers/audioplayers.dart';
@@ -67,14 +6,12 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:path_provider/path_provider.dart';
 import 'dart:convert';
 import 'package:alarm_demo/systemSpecificPaths.dart';
-=======
->>>>>>> origin/master
+
 void callingNotification(int uniqueID) async {
   final prefs = await SharedPreferences.getInstance();
   final value = prefs.getString(uniqueID.toString());
   var temp = json.decode(value);
   print("Ima ");
-  prefs.setString("demo", "12347");
   print(uniqueID);
   print(uniqueID.runtimeType);
   var schedule = uniqueID.toString().substring(
@@ -91,13 +28,16 @@ void callingNotification(int uniqueID) async {
       initializationSettings,
     );
 
+    // This code is for showing notification. Note that this section of code is not working in release mode
     var platform = await notificationSettings(temp["image"]);
     await flutterLocalNotificationsPlugin.show(
         0, "Fitness traker", "${temp["label"]}", platform,
         payload: "You have clicked notification");
+
+    //This code is for playing audio
     AudioPlayer audioPlayer2 = AudioPlayer();
     Directory appDocDirectory;
-    appDocDirectory = await systemSpecificFilePath();
+    appDocDirectory = await systemSpecificFilePath();   
     var audioFilePath =
         appDocDirectory.path +
             "/${uniqueID.toString()}" +
@@ -106,10 +46,8 @@ void callingNotification(int uniqueID) async {
     await audioPlayer2.play(audioFilePath, isLocal: true);
     final DateTime now = DateTime.now();
   } 
-  // This condtion is not used now. Ignore it for a while
+  // Ignore this else condtion for a while
   else {
-    print(schedule);
-    print("not laone");
     schedule.runes.forEach((int rune) async {
       var character = new String.fromCharCode(rune);
       //Checking for selected days for alarm
